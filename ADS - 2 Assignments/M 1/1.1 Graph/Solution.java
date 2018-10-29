354cc38d87a6bb70c01e0ceffc4c67e1ab87413a
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-10-29 19:54:33 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/M 1/1.1 Graph/Solution.java
+++ ADS - 2 Assignments/M 1/1.1 Graph/Solution.java
@@ -1,5 +1,10 @@
+/**
+ * Scanner import.
+ */
 import java.util.Scanner;
-
+/**
+ * Interface for graph.
+ */
 interface Graph {
     public int vertex();
     public int edge();
@@ -7,14 +12,33 @@ interface Graph {
     public Iterable<Integer> list(int v);
     public boolean hasEdge(int v, int w);
 }
-
+/**
+ * Class for graph theory.
+ */
 class GraphTheory implements Graph {
+    /**
+     * Vertices.
+     */
     private int v;
+    /**
+     * Edges.
+     */
     private int e;
+    /**
+     * Graph array.
+     */
     private Bag<Integer>[] list;
+    /**
+     * Constructs the object.
+     */
     GraphTheory() {
-
+        //Empty constructor.
     }
+    /**
+     * Constructs the object.
+     *
+     * @param      v1    The v 1
+     */
     public GraphTheory(int v1) {
         this.v = v1;
         this.e = 0;
@@ -23,15 +47,29 @@ class GraphTheory implements Graph {
             list[i] = new Bag<Integer>();
         }
     }
-
+    /**
+     * Vertices count.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int vertex() {
         return v;
     }
-
+    /**
+     * Edges count.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int edge() {
         return e;
     }
-
+    /**
+     * Adds an edge.
+     * Complexity of add is 1.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
     public void addEdge(int v, int w) {
         if (v == w) {
             return;
@@ -44,11 +82,28 @@ class GraphTheory implements Graph {
         list[v].add(w);
         list[w].add(v);
     }
-
+    /**
+     * Particular value in the list.
+     * Complexity is 1.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Iterable<Integer> list(int v) {
         return list[v];
     }
-
+    /**
+     * Determines if it has edge.
+     * Checks if there is a connection between two nodes.
+     * Complexity is N.
+     * Checks throughout the list for the particular index.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
     public boolean hasEdge(int v, int w) {
         for (int k : list[v]) {
             if (k == w) {
@@ -57,13 +112,24 @@ class GraphTheory implements Graph {
         }
         return false;
     }
-
+    /**
+     * Displaying the given list in list format.
+     * Complexity is N.
+     *
+     * @param      v          { Vertices }
+     * @param      e          { Edges }
+     * @param      tokens     The tokens
+     *
+     * @throws     Exception  { No edges Exception }
+     */
     public void listdisplay(int v, int e, String[] tokens) throws Exception {
         if (e <= 1 && v <= 1) {
-            System.out.println(vertex() + " vertices" + ", " + edge() + " edges");
+            System.out.println(vertex() + " vertices"
+                + ", " + edge() + " edges");
             throw new Exception("No edges");
         } else {
-            System.out.println(vertex() + " vertices" + ", " + edge() + " edges");
+            System.out.println(vertex() + " vertices"
+                + ", " + edge() + " edges");
             for (int i = 0; i < tokens.length; i++) {
             String str = "";
             str = tokens[i] + ": ";
@@ -74,13 +140,24 @@ class GraphTheory implements Graph {
             }
         }
     }
-
+    /**
+     * Display the given list in matix format.
+     * Complexity is N^2.
+     * The array used is a double dimensional array.
+     *
+     * @param      v          { Vertices }
+     * @param      e          { Edges }
+     *
+     * @throws     Exception  { No edges }
+     */
     public void matrixdisplay(int v, int e) throws Exception {
         if (e <= 1 && v <= 1) {
-            System.out.println(vertex() + " vertices" + ", " + edge() + " edges");
+            System.out.println(vertex() + " vertices"
+                + ", " + edge() + " edges");
             throw new Exception("No edges");
         } else {
-            System.out.println(vertex() + " vertices" + ", " + edge() + " edges");
+            System.out.println(vertex() + " vertices"
+                + ", " + edge() + " edges");
             int[][] disp = new int[v][v];
             for (int i = 0; i  < v; i++) {
                 for (int j = 0; j < v; j++) {
@@ -99,12 +176,21 @@ class GraphTheory implements Graph {
         }
     }
 }
-
-class Solution {
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
     Solution() {
 
     }
-
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         GraphTheory grph = null;
@@ -115,7 +201,8 @@ class Solution {
         grph = new GraphTheory(vertex);
         while (sc.hasNextLine()) {
             String[] connect = sc.nextLine().split(" ");
-            grph.addEdge(Integer.parseInt(connect[0]), Integer.parseInt(connect[1]));
+            grph.addEdge(Integer.parseInt(connect[0]),
+                Integer.parseInt(connect[1]));
         }
 
         switch (type) {
