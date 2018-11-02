165d2925add8496d8610248e20d0cec163d09c50
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-11-02 09:41:08 +0530

    "ads2_m5_ba1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS - 2 Assignments/M 5/Bonus-1/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+final class Solution {
+	private Solution() {
+		//Empty constructor.
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] val = sc.nextLine().split(" ");
+		int vertex = Integer.parseInt(val[0]);
+		int edge = Integer.parseInt(val[1]);
+		Graph grp = new Graph(vertex);
+		while (sc.hasNextLine()) {
+			String[] nodes = sc.nextLine().split(" ");
+			grp.addEdge(Integer.parseInt(nodes[0]), Integer.parseInt(nodes[1]));
+		}
+		System.out.println(grp);
+	}
+}
