8b208b6dd00b374da0a7a527207161fbb455f84f
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-11-04 12:55:05 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/M 4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS - 2 Assignments/M 4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,6 +1,28 @@
+/**
+ * Files import.
+ */
 import java.io.File;
+/**
+ * Scanner import.
+ */
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty Constructor.
+    }
+    /**
+     * Main function.
+     *
+     * @param      args       The arguments
+     *
+     * @throws     Exception  { exception_description }
+     */
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
         String synsets = sc.nextLine();
@@ -26,7 +48,9 @@ class Solution {
                     String[] tokens = sc.nextLine().split(" ");
                     try {
                         wrdnet.sap(tokens[0], tokens[1]);
-                        System.out.println("distance = " + wrdnet.distance(tokens[0], tokens[1]) + ", ancestor = " + wrdnet.sap(tokens[0], tokens[1]));
+                        System.out.println("distance = "
+                            + wrdnet.distance(tokens[0], tokens[1])
+                            + ", ancestor = " + wrdnet.sap(tokens[0], tokens[1]));
                     } catch (Exception e) {
                         System.out.println("IllegalArgumentException");
                     }
