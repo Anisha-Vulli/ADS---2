765e42b51ab3a260a84ae946bd87ef2be9877652
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-11-06 17:38:36 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 2 Assignments/M 8/1. Shortest Paths/Solution.java
+++ ADS - 2 Assignments/M 8/1. Shortest Paths/Solution.java
@@ -2,14 +2,25 @@
  * Scanner import.
  */
 import java.util.Scanner;
-
-class Solution {
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+final class Solution {
+	private Solution() {
+		//Empty constructor.
+	}
+	/**
+	 * Main function.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String[] ints = sc.nextLine().split(" ");
 		int vertex = Integer.parseInt(ints[0]);
 		int edges = Integer.parseInt(ints[1]);
-		BinarySearchST<String, Integer> bst = new BinarySearchST<String, Integer>();
+		BinarySearchST<String, Integer> bst
+		= new BinarySearchST<String, Integer>();
 		String[] vertices = sc.nextLine().split(" ");
 		for (int i = 0; i < vertices.length; i++) {
 			bst.put(vertices[i], i);
@@ -18,7 +29,8 @@ class Solution {
 		EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertex);
 		for (int i = 0; i < edges; i++) {
 			String[] tokens = sc.nextLine().split(" ");
-			e = new Edge(bst.get(tokens[0]), bst.get(tokens[1]), Double.parseDouble(tokens[2]));
+			e = new Edge(bst.get(tokens[0]),
+				bst.get(tokens[1]), Double.parseDouble(tokens[2]));
 			ewg.addEdge(e);
 		}
 		int queries = Integer.parseInt(sc.nextLine());
