bb8903ef9af667b355b712835fa39d926a82d748
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-11-08 14:04:15 +0530

    "ads2_m10_a1_1: 4/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/M 10/Seam Carving/Solution.java
+++ ADS - 2 Assignments/M 10/Seam Carving/Solution.java
@@ -44,7 +44,7 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String cases = scan.nextLine();
         SeamCarver seamCarver = null;
-        if (!scan.hasNextLine()) {
+        if (!(scan.hasNextLine())) {
             System.out.println("Picture is null");
             return;
         }
