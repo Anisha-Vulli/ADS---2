063514866df716bb83dda69b36b439718195c2fc
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-11-10 12:12:01 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 2 Assignments/M 12/ADS-2_Exam-2/DirectedEdge.java
+++ ADS - 2 Assignments/M 12/ADS-2_Exam-2/DirectedEdge.java
@@ -32,7 +32,7 @@ public class EdgeWeightedDigraph {
      * @throws IllegalArgumentException if
      * {@code V < 0}
      */
-    public EdgeWeightedDigraph(int vertexcnt) {
+    public EdgeWeightedDigraph(final int vertexcnt) {
         if (V < 0) {
             throw new IllegalArgumentException
             ("Number of vertices in a Digraph must be nonnegative");
@@ -46,16 +46,20 @@ public class EdgeWeightedDigraph {
     }
 
     /**
-     * Initializes a random edge-weighted digraph with {@code V} vertices and <em>E</em> edges.
+     * Initializes a random edge-weighted digraph with
+     * {@code V} vertices and <em>E</em> edges.
      *
      * @param  V the number of vertices
      * @param  E the number of edges
      * @throws IllegalArgumentException if {@code V < 0}
      * @throws IllegalArgumentException if {@code E < 0}
      */
-    public EdgeWeightedDigraph(int V, int E) {
+    public EdgeWeightedDigraph(final int V, final int E) {
         this(V);
-        if (E < 0) throw new IllegalArgumentException("Number of edges in a Digraph must be nonnegative");
+        if (E < 0) {
+         throw new IllegalArgumentException(
+            "Number of edges in a Digraph must be nonnegative");
+        }
         for (int i = 0; i < E; i++) {
             int v = StdRandom.uniform(V);
             int w = StdRandom.uniform(V);
@@ -66,17 +70,20 @@ public class EdgeWeightedDigraph {
     }
 
     /**  
-     * Initializes an edge-weighted digraph from the specified input stream.
+     * Initializes an edge-weighted digraph from the
+     * specified input stream.
      * The format is the number of vertices <em>V</em>,
      * followed by the number of edges <em>E</em>,
      * followed by <em>E</em> pairs of vertices and edge weights,
      * with each entry separated by whitespace.
      *
      * @param  in the input stream
-     * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
-     * @throws IllegalArgumentException if the number of vertices or edges is negative
+     * @throws IllegalArgumentException if the endpoints of
+     * any edge are not in prescribed range
+     * @throws IllegalArgumentException if the number of
+     * vertices or edges is negative
      */
-    public EdgeWeightedDigraph(In in) {
+    public EdgeWeightedDigraph(final In in) {
         this(in.readInt());
         int E = in.readInt();
         if (E < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
@@ -101,8 +108,10 @@ public class EdgeWeightedDigraph {
         for (int v = 0; v < G.V(); v++)
             this.indegree[v] = G.indegree(v);
         for (int v = 0; v < G.V(); v++) {
-            // reverse so that adjacency list is in same order as original
-            Stack<DirectedEdge> reverse = new Stack<DirectedEdge>();
+            // reverse so that adjacency list
+            //is in same order as original
+            Stack<DirectedEdge> reverse
+            = new Stack<DirectedEdge>();
             for (DirectedEdge e : G.adj[v]) {
                 reverse.push(e);
             }
@@ -113,37 +122,50 @@ public class EdgeWeightedDigraph {
     }
 
     /**
-     * Returns the number of vertices in this edge-weighted digraph.
+     * Returns the number of vertices
+     * in this edge-weighted digraph.
      *
-     * @return the number of vertices in this edge-weighted digraph
+     * @return the number of vertices
+     * in this edge-weighted digraph
      */
     public int V() {
         return V;
     }
 
     /**
-     * Returns the number of edges in this edge-weighted digraph.
+     * Returns the number of edges in this
+     * edge-weighted digraph.
      *
-     * @return the number of edges in this edge-weighted digraph
+     * @return the number of edges in this
+     * edge-weighted digraph
      */
     public int E() {
         return E;
     }
 
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    /**
+     * throw an IllegalArgumentException unless
+     * {@code 0 <= v < V}.
+     *
+     * @param      v     { parameter_description }
+     */
     private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException(
+                "vertex " + v + " is not between 0 and " + (V-1));
+        }
     }
 
     /**
-     * Adds the directed edge {@code e} to this edge-weighted digraph.
+     * Adds the directed edge {@code e} to
+     * this edge-weighted digraph.
      *
      * @param  e the edge
-     * @throws IllegalArgumentException unless endpoints of edge are between {@code 0}
+     * @throws IllegalArgumentException unless
+     * endpoints of edge are between {@code 0}
      *         and {@code V-1}
      */
-    public void addEdge(DirectedEdge e) {
+    public void addEdge(final DirectedEdge e) {
         int v = e.from();
         int w = e.to();
         validateVertex(v);
@@ -155,49 +177,61 @@ public class EdgeWeightedDigraph {
 
 
     /**
-     * Returns the directed edges incident from vertex {@code v}.
+     * Returns the directed edges incident from
+     * vertex {@code v}.
      *
      * @param  v the vertex
-     * @return the directed edges incident from vertex {@code v} as an Iterable
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @return the directed edges incident from vertex
+     * {@code v} as an Iterable
+     * @throws IllegalArgumentException unless
+     * {@code 0 <= v < V}
      */
-    public Iterable<DirectedEdge> adj(int v) {
+    public Iterable<DirectedEdge> adj(final int v) {
         validateVertex(v);
         return adj[v];
     }
 
     /**
-     * Returns the number of directed edges incident from vertex {@code v}.
-     * This is known as the <em>outdegree</em> of vertex {@code v}.
+     * Returns the number of directed edges incident
+     * from vertex {@code v}.
+     * This is known as the <em>outdegree</em> of
+     * vertex {@code v}.
      *
      * @param  v the vertex
      * @return the outdegree of vertex {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @throws IllegalArgumentException unless
+     * {@code 0 <= v < V}
      */
-    public int outdegree(int v) {
+    public int outdegree(final int v) {
         validateVertex(v);
         return adj[v].size();
     }
 
     /**
-     * Returns the number of directed edges incident to vertex {@code v}.
-     * This is known as the <em>indegree</em> of vertex {@code v}.
+     * Returns the number of directed edges incident
+     * to vertex {@code v}.
+     * This is known as the <em>indegree</em> of
+     * vertex {@code v}.
      *
      * @param  v the vertex
      * @return the indegree of vertex {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @throws IllegalArgumentException unless
+     * {@code 0 <= v < V}
      */
-    public int indegree(int v) {
+    public int indegree(final int v) {
         validateVertex(v);
         return indegree[v];
     }
 
     /**
-     * Returns all directed edges in this edge-weighted digraph.
-     * To iterate over the edges in this edge-weighted digraph, use foreach notation:
+     * Returns all directed edges in this edge-weighted
+     * digraph.
+     * To iterate over the edges in this edge-weighted
+     * digraph, use foreach notation:
      * {@code for (DirectedEdge e : G.edges())}.
      *
-     * @return all edges in this edge-weighted digraph, as an iterable
+     * @return all edges in this edge-weighted digraph,
+     * as an iterable
      */
     public Iterable<DirectedEdge> edges() {
         Bag<DirectedEdge> list = new Bag<DirectedEdge>();
@@ -210,10 +244,13 @@ public class EdgeWeightedDigraph {
     } 
 
     /**
-     * Returns a string representation of this edge-weighted digraph.
+     * Returns a string representation of this edge
+     * weighted digraph.
      *
-     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
-     *         followed by the <em>V</em> adjacency lists of edges
+     * @return the number of vertices <em>V</em>,
+     * followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency
+     *         lists of edges
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
@@ -227,40 +264,4 @@ public class EdgeWeightedDigraph {
         }
         return s.toString();
     }
-
-    // /**
-    //  * Unit tests the {@code EdgeWeightedDigraph} data type.
-    //  *
-    //  * @param args the command-line arguments
-    //  */
-    // public static void main(String[] args) {
-    //     In in = new In(args[0]);
-    //     EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
-    //     StdOut.println(G);
-    // }
-
 }
-
-/******************************************************************************
- *  Copyright 2002-2018, Robert Sedgewick and Kevin Wayne.
- *
- *  This file is part of algs4.jar, which accompanies the textbook
- *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
- *
- *
- *  algs4.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  algs4.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
