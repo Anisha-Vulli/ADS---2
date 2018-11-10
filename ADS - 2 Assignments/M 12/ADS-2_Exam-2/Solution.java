17e0f7f8fd4af35cb01175897b335d35b4e00b3b
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-11-10 11:57:45 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 2 Assignments/M 12/ADS-2_Exam-2/Solution.java
+++ ADS - 2 Assignments/M 12/ADS-2_Exam-2/Solution.java
@@ -46,28 +46,58 @@ public class Solution {
             // third is the destination.
             // If the path exists print the distance between them.
             // Other wise print "No Path Found."
-            String[] inputval = sc.nextLine().split(" ");
-            int source1 = Integer.parseInt(inputval[0]);
-            int destination = Integer.parseInt(inputval[1]);
-            int via = Integer.parseInt(inputval[2]);
-            DijkstraUndirectedSP djksp = new DijkstraUndirectedSP(ewg, source1);
-            if (djksp.hasPathTo(destination)) {
-                DijkstraUndirectedSP dusp = new DijkstraUndirectedSP(ewg, via);
-                for (Edge edg : dusp.pathTo(destination)) {
-                    if (edg.either() != via) {
-                        break;
+            String[] paths = sc.nextLine().split(" ");
+            source = Integer.parseInt(paths[0]);
+            int via = Integer.parseInt(paths[1]);
+            int destination = Integer.parseInt(paths[2]);
+            DijkstraUndirectedSP dusp = new DijkstraUndirectedSP(ewg, source);
+            if (dusp.hasPathTo(destination)) {
+                Queue<Integer> q = new Queue<Integer>();
+                for (Edge ed : dusp.pathTo(via)) {
+                    String[] edges = ed.toString().split(" ");
+                    String[] vertices = edges[0].split("-");
+                    int m = 0;
+                    int n = 0;
+                    for (Integer k : q) {
+                        if (Integer.parseInt(vertices[1]) == k) {
+                            m = 1;
+                        }
+                        if (Integer.parseInt(vertices[1]) == k) {
+                            n = 1;
+                        }
                     }
-                    if (edg == null) {
-                        System.out.println("No Path Found.");
-                        break;
+                    if (n == 0) {
+                        q.enqueue(Integer.parseInt(vertices[1]));
                     }
-                    //System.out.print(edg + "   ");
+                    if (m == 0) {
+                        q.enqueue(Integer.parseInt(vertices[0]));
+                    }
                 }
-                for (Edge ed : djksp.pathTo(via)) {
-                    //System.out.print(ed + "   ");
+                DijkstraUndirectedSP dusp1 = new DijkstraUndirectedSP(ewg, via);
+                for (Edge edg : dusp1.pathTo(destination)) {
+                    String[] edgess = edg.toString().split(" ");
+                    String[] vertexs = edgess[0].split("-");
+                    int p = 0;
+                    int r = 0;
+                    for (Integer j : q) {
+                        if (Integer.parseInt(vertexs[0]) == j) {
+                            p = 1;
+                        }
+                        if (Integer.parseInt(vertexs[1]) == j) {
+                            r = 1;
+                        }
+                    }
+                    if (p == 0) {
+                        q.enqueue(Integer.parseInt(vertexs[0]));
+                    }
+                    if (r == 0) {
+                        q.enqueue(Integer.parseInt(vertexs[1]));
+                    }
                 }
-                
-                System.out.println(djksp.distTo(via) + dusp.distTo(destination));
+                System.out.println(dusp.distTo(via) + dusp1.distTo(destination));
+                while (!q.isEmpty()) {
+                    System.out.print(q.dequeue() + " ");
+                }
             } else {
                 System.out.println("No Path Found.");
             }
