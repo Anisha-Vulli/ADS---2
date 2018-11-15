c2a6ca57f46242faacec4c912e646a65b55a04c4
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-11-15 16:50:26 +0530

    "ads2_m16_a1_1: 2/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 2 Assignments/M 16/boggle/BoggleSolver.java
+++ ADS - 2 Assignments/M 16/boggle/BoggleSolver.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class BoggleSolver {
     private TST<Integer> tst;
     // Initializes the data structure using the given array of strings as the dictionary.
@@ -11,7 +12,34 @@ public class BoggleSolver {
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
+        boolean[][] marked = new boolean[board.rows()][board.cols()];
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < board.rows(); i++) {
+            for (int j = 0; j < board.cols(); j++) {
+                marked[i][j] = true;
+                String letter = "" + board.getLetter(i, j);
+                dfs(board, list, i, j, marked, letter);
+            }
+        }
         return new Bag<String>();
+    }
+
+    public void dfs(BoggleBoard board, ArrayList<String> list, int i, int j, boolean[][] marked, String letter) {
+        if (tst.contains(letter) && letter.length() > 2) {
+            list.add(letter);
+        }
+
+        for (int row = 0; row < board.rows(); row++) {
+            for (int col = 0; col < board.cols(); col++) {
+                if (marked[i][j]) {
+                    continue;
+                }
+
+                marked[i][j] = true;
+                letter += board.getLetter(row, col);
+                dfs(board, list, row, col, marked, letter);
+            }
+        }
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
@@ -30,4 +58,5 @@ public class BoggleSolver {
         }
         return 0;
     }
+
 }
